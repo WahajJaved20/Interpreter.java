0f974d6ec3f10a4ca5915d13de7beb1b0a168d48
Wahaj Javed <wahaj.javed02@gmail.com>
2025-01-28 20:50:46 +0500

    add support for block scopes

--- src/main/java/Environment.java
+++ src/main/java/Environment.java
@@ -3,6 +3,11 @@ import java.util.Map;
 
 public class Environment {
     private final Map<String, Object> values = new HashMap<>();
+    final Environment enclosing;
+    Environment(){
+        enclosing = null;
+    }
+    Environment(Environment enclosing){ this.enclosing = enclosing;}
     void define(String name, Object value) {
         values.put(name, value);
     }
@@ -10,7 +15,7 @@ public class Environment {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
-
+        if (enclosing != null) return enclosing.get(name);
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
@@ -19,7 +24,10 @@ public class Environment {
             values.put(name.lexeme, value);
             return;
         }
-
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
